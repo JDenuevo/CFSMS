6d641a1666dba310728093051c154a626e9f53ae
JDenuevo <denuevojhemar.bsit@gmail.com>
2024-08-07 13:52:58 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,317 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>CSFMS</title>
+
+  <!-- Favicons -->
+  <link href="./assets/img/logowhite.png" rel="icon">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+  
+  <!-- Main Template CSS Files -->
+  <link href="./assets/css/bootstraps.css" rel="stylesheet">
+  <link href="./assets/css/web.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<!-- Navbar Start -->
+<div class="container-fluid fixed-top bg-dark" id="header">
+  <div class="container">
+    <nav class="navbar navbar-expand-lg p-0 justify-content-between" id="navbar">
+      <a href="#home" class="navbar-brand">
+        <img class="img-fluid" src="./assets/img/logowhite.png" width="100px">
+      </a>
+      
+      <!-- Mobile Toggle Button -->
+      <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <!-- Mobile Menu -->
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav d-flex justify-content-end flex-fill">
+          <a href="#features" class="fw-semibold nav-link scrollto active"><i class="fa-solid fa-cube me-2"></i>Features</a>
+          <a href="#about" class="fw-semibold nav-link scrollto"><i class="fa-solid fa-users me-2"></i>About us</a>
+          <a href="#team" class="fw-semibold nav-link scrollto"><i class="fa-solid fa-people-group me-2"></i>Team</a>
+          <a href="#login" class=" fw-semibold nav-link scrollto"><i class="fa-solid fa-circle-user me-2"></i>Login</a>
+        </div>
+      </div>
+    </nav>
+  </div>
+</div>
+<!-- Navbar End -->
+
+<br><br>
+
+<!-- Home Section -->
+<section id="home" class="d-flex align-items-center vh-100">
+  <div class="container pt-5">
+
+    <div class="row">
+      <div class="col-12 col-lg-6 my-auto">
+        <div class="text-start">
+          <h4 class="text-primary">Introducing</h4><br>
+          <h1 class="fw-bold">Classroom and Faculty Schedule Management System</h1><br>
+          <h4 class="text-primary">of University of Caloocan City</h4><br>
+          <p>"Empowering education through seamless organization: where every schedule aligns, and every faculty shines."</p>
+
+        </div>
+       
+      </div>
+      <div class="col-12 col-lg-6">
+        <img src="./assets/img/booking.png" class="img-fluid">
+
+      </div>
+    </div>
+    
+  </div>
+</section>
+<!-- End Home -->
+
+<!-- Features Section -->
+<section id="features" class="features">
+<hr>
+  <div class="container">
+    <div class="row g-0">
+      <div class="col-md">
+        <div class="card shadow-sm h-100 rounded-0">
+          <div class="card-body p-5 my-auto">
+            <img src="./assets/img/planner.png" class="img-fluid w-25">
+            <h3 class="fw-bold my-3">Easy to Schedule</h3>
+            <p class="card-text">A <span class="fw-bold">CFSMS</span> improves scheduling, efficiently distributes resources, and keeps the academic environment in order by simplifying the process and improving communication.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md">
+        <div class="card shadow-sm h-100 rounded-0">
+          <div class="card-body p-5 my-auto">
+            <img src="./assets/img/antivirus.png" class="img-fluid w-25">
+            <h3 class="fw-bold my-3">Reliable Platform</h3>
+            <p class="card-text">A <span class="fw-bold">CFSMS</span> built on a dependable platform guarantees consistent access to correct scheduling information, reduces interruptions, and develops user confidence, all of which contribute to the efficient running of educational facilities.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md">
+        <div class="card shadow-sm h-100 rounded-0">
+          <div class="card-body p-5 my-auto">
+            <img src="./assets/img/design.png" class="img-fluid w-25">
+            <h3 class="fw-bold my-3">Responsive Design</h3>
+            <p class="card-text">A flexible <span class="fw-bold">CFSMS</span> improves accessibility and usability by adjusting to different devices and screen sizes, giving the best viewing and interaction experiences.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div style="background-color: #E2F0E2;">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-12 col-lg-6">
+          <img src="./assets/img/features.png" class="img-fluid">
+
+        </div>
+
+        <div class="col-12 col-lg-6 my-auto">
+          <div class="text-start">
+            <h4 class="fw-bold text-primary">Responsive Design</h4>
+            <h1 class="fw-bold">Best Features of CFSMS</h1>
+            <label class="my-3">A <span class="fw-bold">CFSMS</span> built on a dependable platform guarantees consistent access to correct scheduling information, reduces interruptions, and develops user confidence, all of which contribute to the efficient running of educational facilities.</label>
+            <label class="">A <span class="fw-bold">CFSMS</span> built on a dependable platform guarantees consistent access to correct scheduling information, reduces interruptions, and develops user confidence, all of which contribute to the efficient running of educational facilities.</label>
+        
+          </div>
+        
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+
+  <img src="./assets/img/up.png" class="img-fluid z-n1 position-absolute" width="50px">
+
+  <div class="container py-3">
+  
+  <br>
+
+    <div class="row">
+      
+      <div class="col-12 col-lg-6 my-auto">
+        <div class="text-start">
+          <h4 class="fw-bold text-primary">Responsive Design</h4>
+          <h1 class="fw-bold">Best Features of CFSMS</h1>
+          <label class="my-3">A <span class="fw-bold">CFSMS</span> built on a dependable platform guarantees consistent access to correct scheduling information, reduces interruptions, and develops user confidence, all of which contribute to the efficient running of educational facilities.</label>
+          <label>A <span class="fw-bold">CFSMS</span> built on a dependable platform guarantees consistent access to correct scheduling information, reduces interruptions, and develops user confidence, all of which contribute to the efficient running of educational facilities.</label>
+       
+        </div>
+      
+      </div>
+
+      <div class="col-12 col-lg-6">
+        <img src="./assets/img/date.png" class="img-fluid">
+
+      </div>
+
+    </div>
+
+  </div>
+
+</section>
+<!-- End Features Section -->
+
+<!-- About Us Section -->
+<section id="about" class="about">
+
+  <div class="row g-0">
+
+    <div class="col-12 col-lg-6">
+      <div class="box1 p-5" style="background-color: #E2F0E2;">
+
+        <div class="d-flex justify-content-start my-auto p-5">
+          <img src="./assets/img/ucclogo.png" class="me-2" width="80px">
+          <div class="word">
+            <h5 class="text-primary fw-bold">University of Caloocan City</h5>
+            <h3 class="fw-bold">Vision</h3>
+          </div>
+        </div>
+        
+        <div class="px-5">
+          <p>A local government university with global quality of education imbued with desired knowledge, skills, and values for academic excellence, professional development, civic consciousness, resilient citizenry, technological advancement, ecological sustainability and continual improvement.</p>
+        
+        </div>
+        
+      </div>
+    </div>
+
+    <div class="col-12 col-lg-6 d-none d-lg-block d-xl-block">
+      <div class="flex-grow-1"></div>
+    </div>
+
+  </div>
+
+    
+  <div class="row g-0">
+
+    <div class="col-12 col-lg-6 d-none d-lg-block d-xl-block">
+      <div class="flex-grow-1"></div>
+    </div>
+
+    <div class="col-12 col-lg-6">
+      <div class="box2 p-5" style="background-color: #E2F0E2;">
+
+        <img src="./assets/img/circle.png" class="img-fluid start-0 z-n1 position-absolute">
+
+        <div class="d-flex justify-content-end my-auto p-5">
+          <div class="word">
+            <h5 class="text-primary fw-bold">University of Caloocan City</h5>
+            <h3 class="fw-bold">Mission</h3>
+          </div>
+          <img src="./assets/img/ucclogo.png" class="me-2" width="80px">
+        </div>
+        
+        <div class="text-end px-5">
+          <p>To develop academically excellent, professionally progressive, industry sensitive, environmentally and technologically conscious, globally competitive and resilient graduates through quality instruction, functional co-curricular activities, responsive community immersion programs, intensive research and development and continually improved quality management system molding them to become effective social and cultural agent of change.</p>
+        
+        </div>
+        
+      </div>
+    </div>
+
+  </div>
+
+</section>
+<!-- End About Us Section -->
+
+<!-- Team Section -->
+<section id="team" class="team vh-100">
+
+<img src="./assets/img/wave.png" class="img-fluid w-25 z-n1 start-0 position-absolute">
+
+  <div class="container text-center py-3">
+    <h5 class="fw-bold text-primary">The Team</h5>
+    <h3 class="fw-bold">Built by CFSMS Team</h3>
+    <label>The Classroom and Faculty Schedule Management System Team aims to enhance academic operations by designing a user-friendly, efficient system that streamlines scheduling, improves resource allocation, and promotes better communication among students, instructors, and administrators.</label>
+    
+    <div class="row g-2 py-5">
+      <div class="col-3">
+        <div class="card swiper-slide small-card rounded-5">
+          <div class="image-container rounded-5">
+            <img src="./assets/img/cinderella.png" class="card-img-top rounded" alt="">
+            
+            <div class="card-body text-center text-white position-absolute w-100" style="bottom: 0; background: #FF6C4E;">
+                <label class="fw-bold fs-6">Aniera Joy Salaysay</label>
+                <p class="" style="font-size: 13px;">Programmer (Front End & Back End)</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="card swiper-slide small-card rounded-5">
+          <div class="image-container rounded-5">
+            <img src="./assets/img/cinderella.png" class="card-img-top rounded" alt="">
+            
+            <div class="card-body text-center text-white position-absolute w-100" style="bottom: 0; background: #FF6C4E;">
+                <label class="fw-bold fs-6">Jhoanne Nieves</label>
+                <p class="" style="font-size: 13px;">UI/UX Editor</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="card swiper-slide small-card rounded-5">
+          <div class="image-container rounded-5">
+            <img src="./assets/img/cinderella.png" class="card-img-top rounded" alt="">
+            
+            <div class="card-body text-center text-white position-absolute w-100" style="bottom: 0; background: #FF6C4E;">
+                <label class="fw-bold fs-6">Lyka Jane Hernandez</label>
+                <p class="" style="font-size: 13px;">Documentation</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="card swiper-slide small-card rounded-5">
+          <div class="image-container rounded-5">
+            <img src="./assets/img/cinderella.png" class="card-img-top rounded" alt="">
+            
+            <div class="card-body text-center text-white position-absolute w-100" style="bottom: 0; background: #FF6C4E;">
+                <label class="fw-bold fs-6">Rina Robe Villanueva</label>
+                <p class="" style="font-size: 13px;">Documentation</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+
+</section>
+
+<!-- Footer -->
+<footer id="footer">
+  <div class="container sticky-bottom pb-5">
+    
+    <hr style="color: black; border-width: 3px;">
+
+    <label>2023 - Webapp_UCC - <a href="">Privacy</a></label>
+
+  </div>
+</footer>
+<!-- End Footer  -->
+ 
+  <!-- Template Main JS File -->
+  <script src="./assets/js/bootstrap.bundle.js"></script>
+  <script src="./assets/js/main.js"></script>
+
+</body>
+</html>
